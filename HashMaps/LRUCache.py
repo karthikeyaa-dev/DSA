fd80b69ec408c197c7bfafa49e7015c818ca9fdc
Kathikeya <k.tallapaneni@uptitude.cloud>
2025-07-16 23:55:16 +0530

    Added LRUCache problem

---
+++ HashMaps/LRUCache.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}  # key -> Node
+        # Dummy head and tail
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        """Remove node from list."""
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def _add_to_front(self, node):
+        """Add node right after head."""
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add_to_front(node)
+            return node.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = Node(key, value)
+        self.cache[key] = node
+        self._add_to_front(node)
+
+        if len(self.cache) > self.capacity:
+            # Remove from tail
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
