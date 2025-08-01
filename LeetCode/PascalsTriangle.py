5ebfc9537bb23b4591258de2aed46b506045d2eb
Karthikeya <karthikeyaa.official@gmail.com>
2025-08-01 23:38:47 +0100

    Updated the code with some changes

---
+++ LeetCode/PascalsTriangle.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def generate(self, numRows):
+        triangle = []
+        for row_num in range(numRows):
+            row = [1] * (row_num + 1)
+            for j in range(1, row_num):
+                row[j] = triangle[row_num - 1][j - 1] + triangle[row_num - 1][j]
+            triangle.append(row)
+        return triangle
