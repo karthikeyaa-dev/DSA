49106cc9f247c10377c62c6044f7894b29ab76fa
Karthikeya <karthikeyaa.official@gmail.com>
2025-08-04 09:45:15 +0100

    Added new twu sum problem with optimized solution

---
+++ Arrays/TwoSumProblem/TwoSum_BruteForce.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        l = len(nums)
+        for i in range(l):
+            for j in range(i+1, l):
+                if nums[i] + nums[j] == target:
+                    return i, j
+
