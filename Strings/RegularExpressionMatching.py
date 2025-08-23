ce0b73ba7c9d0b9620f9a9e013c4ef703b8f620a
Karthikeya <karthikeyaa.official@gmail.com>
2025-08-23 23:44:51 +0530

    Solved regular expression problem with dynamic approach

---
+++ Strings/RegularExpressionMatching.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+        
+        # dp[i][j] = does s[0:i] match p[0:j]
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        dp[0][0] = True  # Empty pattern matches empty string
+
+        # Fill in patterns like a*, a*b*, a*b*c* that can match empty string
+        for j in range(2, n + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 2]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == s[i - 1] or p[j - 1] == '.':
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    # Zero occurrence
+                    dp[i][j] = dp[i][j - 2]
+                    # One or more, if preceding char matches
+                    if p[j - 2] == s[i - 1] or p[j - 2] == '.':
+                        dp[i][j] |= dp[i - 1][j]
+
+        return dp[m][n]       
