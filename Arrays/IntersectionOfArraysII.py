95c86e7e427979267408aac89f3b961cadc8869a
Karthikeya <karthikeyaa.official@gmail.com>
2025-09-03 11:20:16 +0530

    solved intersection of two arrays

---
+++ Arrays/IntersectionOfArraysII.py
@@ -0,0 +1,11 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        counts = Counter(nums1)  # Count frequency of nums1 elements
+        result = []
+
+        for num in nums2:
+            if counts[num] > 0:
+                result.append(num)
+                counts[num] -= 1  # Decrement count after using an element
+
+        return result
