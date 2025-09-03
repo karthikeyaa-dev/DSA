119970bb672e1c234f4dc983a1e39b7ef3550650
Karthikeya <karthikeyaa.official@gmail.com>
2025-09-03 23:20:30 +0530

    Added the import statements

--- Arrays/IntersectionOfArraysII.py
+++ Arrays/IntersectionOfArraysII.py
@@ -1,3 +1,5 @@
+from typing import List
+from collections import Counter
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         counts = Counter(nums1)  # Count frequency of nums1 elements
